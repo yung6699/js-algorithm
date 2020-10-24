e84afacddd4233b9a96f3a2c82720a9b794dea7b
Yoon <yty3592@gmail.com>
2020-10-24 11:39:39 +0900

    두수의합 문제 주석 추가

--- array/twoSum.js
+++ array/twoSum.js
@@ -1,5 +1,5 @@
 export function twoSum(nums, target) {
-  let map = new Map();
+  let map = new Map(); // hash 문제일때 Map을 떠올려라.
   for (let i = 0; i < nums.length; i++) {
     const temp = target - nums[i];
     if (map.has(temp)) return [map.get(temp), i];
