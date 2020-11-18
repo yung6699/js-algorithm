2f6ed81c57fc07be58a11cb4046ac3224f9238f5
Yoon <yty3592@gmail.com>
2020-11-19 00:27:44 +0900

    슬라이싱 윈도우 내부 최대값 찾는 문제 추가

---
+++ slidingWindow/maxSlidingWindow.js
@@ -0,0 +1,21 @@
+var maxSlidingWindow = function(nums, k) {
+  const initNum = -Infinity;
+  const results = [];
+  const queue = [];
+  let maxNumber = initNum;
+
+  for (let i = 0; i < nums.length; i++) {
+    queue.push(nums[i]);
+    if (i < k - 1) continue;
+
+    if (maxNumber === initNum) maxNumber = Math.max(...queue);
+    else if (nums[i] > maxNumber) maxNumber = nums[i];
+    results.push(maxNumber);
+
+    const firstElement = queue.shift();
+    if (maxNumber === firstElement)  maxNumber = initNum;
+
+  }
+
+  return results;
+}
